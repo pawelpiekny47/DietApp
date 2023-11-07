51658350b30200ee31a944bef30d9f5898eec613
pawcio <pawcio.p@wp.pl>
2023-11-07 18:40:06 +0100

    Code refactor

--- app/src/main/java/com/example/dietapp/ui/ingredient/navigation/IngredientNavHost.kt
+++ app/src/main/java/com/example/dietapp/ui/ingredient/navigation/IngredientNavHost.kt
@@ -1,7 +1,5 @@
 package com.example.dietapp.ui.ingredient.navigation
 
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.lifecycle.viewmodel.compose.viewModel
