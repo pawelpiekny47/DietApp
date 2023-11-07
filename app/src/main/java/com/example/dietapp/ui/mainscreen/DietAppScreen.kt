c0a80b828c62f49c30516d0cc541b180f58b3d4e
pawcio <pawcio.p@wp.pl>
2023-11-07 16:48:24 +0100

    Dummy NavigationDrawer added

--- app/src/main/java/com/example/dietapp/ui/mainscreen/DietAppScreen.kt
+++ app/src/main/java/com/example/dietapp/ui/mainscreen/DietAppScreen.kt
@@ -1,25 +1,37 @@
 package com.example.dietapp.ui.mainscreen
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.dietapp.ui.AppViewModelProvider
 import com.example.dietapp.ui.ingredient.navigation.IngredientNavHost
+import kotlinx.coroutines.launch
 
 enum class DietAppScreen(val title: String) {
     IngredientListScreen(title = " Ingredient list"),
@@ -29,6 +41,7 @@ enum class DietAppScreen(val title: String) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DietAppTopBar(
+    menuButtonAction: () -> Unit,
     currentScreen: DietAppScreen,
     canNavigateBack: Boolean,
     navigateUp: () -> Unit,
@@ -48,6 +61,14 @@ fun DietAppTopBar(
                         contentDescription = "Back"
                     )
                 }
+            }
+        },
+        actions = {
+            IconButton(onClick = menuButtonAction) {
+                Icon(
+                    imageVector = Icons.Filled.List,
+                    contentDescription = "Menu"
+                )
             }
         }
     )
@@ -78,25 +99,58 @@ fun FloatButton(
 fun DietAppScreen(
     viewModel: MainScreenViewModel = viewModel(factory = AppViewModelProvider.Factory),
 ) {
-    Scaffold(
-        topBar = {
-            DietAppTopBar(
-                currentScreen = viewModel.topBarName,
-                canNavigateBack = viewModel.canNavigateBack,
-                navigateUp = viewModel.navigateUp
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                Text("Menu", modifier = Modifier.padding(16.dp))
+                Divider()
+                NavigationDrawerItem(
+                    label = { Text(text = "Ingredients") },
+                    selected = false,
+                    onClick = { /*TODO*/ }
+                )
+                NavigationDrawerItem(
+                    label = { Text(text = "Dish") },
+                    selected = false,
+                    onClick = { /*TODO*/ }
+                )
+                NavigationDrawerItem(
+                    label = { Text(text = "Diet settings") },
+                    selected = false,
+                    onClick = { /*TODO*/ }
+                )
+                // ...other drawer items
+            }
+        },
+    ) {
+        Scaffold(
+            topBar = {
+                DietAppTopBar(
+                    menuButtonAction = {
+                        scope.launch {
+                            drawerState.open()
+                        }
+                    },
+                    currentScreen = viewModel.topBarName,
+                    canNavigateBack = viewModel.canNavigateBack,
+                    navigateUp = viewModel.navigateUp
+                )
+            },
+            floatingActionButton = {
+                FloatButton(viewModel.visibleFloatButton, viewModel.floatButtonOnClick)
+            },
+        ) { innerPadding ->
+            IngredientNavHost(
+                innerPadding,
+                viewModel::updateVisibleFloatButton,
+                viewModel::updateTopBarName,
+                viewModel::updateCanNavigateBack,
+                viewModel::updateNavigateUp,
+                viewModel::updateFloatButtonOnClick
             )
-        },
-        floatingActionButton = {
-            FloatButton(viewModel.visibleFloatButton, viewModel.floatButtonOnClick)
-        },
-    ) { innerPadding ->
-        IngredientNavHost(
-            innerPadding,
-            viewModel::updateVisibleFloatButton,
-            viewModel::updateTopBarName,
-            viewModel::updateCanNavigateBack,
-            viewModel::updateNavigateUp,
-            viewModel::updateFloatButtonOnClick
-        )
+        }
     }
 }
